a6784e13ee56b91527dbceba10cef3a86c11e296
Leo <markos_193@hotmail.com>
2017-04-01 23:45:10 -0600

    inyección por constructor

---
+++ src/main/java/com/leo/beans/Persona.java
@@ -0,0 +1,46 @@
+package com.leo.beans;
+
+public class Persona {
+	private int id;
+	private String nombre;
+	private String apodo;
+
+	public Persona(int id, String nombre, String apodo) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apodo = apodo;
+	}
+
+	public Persona(int id) {
+		this.id = id;
+	}
+
+	public Persona(String apodo) {
+		this.apodo = apodo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApodo() {
+		return apodo;
+	}
+
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+
+}
--- src/main/java/com/leo/spring/App.java
+++ src/main/java/com/leo/spring/App.java
@@ -3,7 +3,7 @@ package com.leo.spring;
 import com.leo.beans.AppConfig;
 import com.leo.beans.AppConfig2;
 import com.leo.beans.Mundo;
-
+import com.leo.beans.Persona;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,7 +17,8 @@ public class App {
 //		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
 //		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
 //		ApplicationContext appContext = new AnnotationConfigApplicationContext();
-		
+		/*
+		 * Anotaciones
 		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
 		
 		appContext.register(AppConfig.class);
@@ -29,6 +30,11 @@ public class App {
 		
 		System.out.println(m.getSaludo());
 		
+		 */
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
+		Persona per = (Persona) appContext.getBean("persona");
+		
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo());
 		((ConfigurableApplicationContext)appContext).close();
 
 	}
