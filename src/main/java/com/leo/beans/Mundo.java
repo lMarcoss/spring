758acdc0147b557f6063ab9fbaceb6300b0ce873
Leo <markos_193@hotmail.com>
2017-04-01 23:24:23 -0600

    Utilizando anotaciones

--- src/main/java/com/leo/beans/Mundo.java
+++ src/main/java/com/leo/beans/Mundo.java
@@ -1,7 +1,10 @@
 package com.leo.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Mundo {
 	
+	@Value("Hola mundo!")
 	private String saludo;
 	
 	public String getSaludo(){
--- src/main/java/com/leo/spring/App.java
+++ src/main/java/com/leo/spring/App.java
@@ -1,18 +1,31 @@
 package com.leo.spring;
 
+import com.leo.beans.AppConfig;
+import com.leo.beans.AppConfig2;
 import com.leo.beans.Mundo;
 
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
+//		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
+//		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
+//		ApplicationContext appContext = new AnnotationConfigApplicationContext();
+		
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		
+		appContext.register(AppConfig.class);
+		appContext.register(AppConfig2.class);
+		appContext.refresh();
+
 		Mundo m = (Mundo) appContext.getBean("mundo");
+		
 		
 		System.out.println(m.getSaludo());
 		
