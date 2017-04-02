f460f8e106740ccbd79b6aa512ac69deeef72102
Leo <markos_193@hotmail.com>
2017-04-02 00:30:20 -0600

    inyección con objetos

--- src/main/java/com/leo/beans/Persona.java
+++ src/main/java/com/leo/beans/Persona.java
@@ -4,20 +4,7 @@ public class Persona {
 	private int id;
 	private String nombre;
 	private String apodo;
-
-	public Persona(int id, String nombre, String apodo) {
-		this.id = id;
-		this.nombre = nombre;
-		this.apodo = apodo;
-	}
-
-	public Persona(int id) {
-		this.id = id;
-	}
-
-	public Persona(String apodo) {
-		this.apodo = apodo;
-	}
+	private Pais pais;
 
 	public int getId() {
 		return id;
@@ -41,6 +28,14 @@ public class Persona {
 
 	public void setApodo(String apodo) {
 		this.apodo = apodo;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
 	}
 
 }
--- src/main/java/com/leo/spring/App.java
+++ src/main/java/com/leo/spring/App.java
@@ -13,29 +13,44 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 
 	public static void main(String[] args) {
-		
-//		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
-//		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
-//		ApplicationContext appContext = new AnnotationConfigApplicationContext();
-		/*
-		 * Anotaciones
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
-		
-		appContext.register(AppConfig.class);
-		appContext.register(AppConfig2.class);
-		appContext.refresh();
 
-		Mundo m = (Mundo) appContext.getBean("mundo");
-		
-		
-		System.out.println(m.getSaludo());
-		
+		// ApplicationContext appContext = new
+		// ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
+		// ApplicationContext appContext = new
+		// AnnotationConfigApplicationContext(AppConfig.class,
+		// AppConfig2.class);
+		// ApplicationContext appContext = new
+		// AnnotationConfigApplicationContext();
+		/*
+		 * Anotaciones AnnotationConfigApplicationContext appContext = new
+		 * AnnotationConfigApplicationContext();
+		 * 
+		 * appContext.register(AppConfig.class);
+		 * appContext.register(AppConfig2.class); appContext.refresh();
+		 * 
+		 * Mundo m = (Mundo) appContext.getBean("mundo");
+		 * 
+		 * 
+		 * System.out.println(m.getSaludo());
+		 * 
+		 */
+
+		/*
+		 * Inyeccion por constructor
+		 * 
+		 * ApplicationContext appContext = new
+		 * ClassPathXmlApplicationContext("com/leo/xml/beans.xml"); Persona per
+		 * = (Persona) appContext.getBean("persona");
+		 * 
+		 * System.out.println(per.getId() + " " + per.getNombre() + " " +
+		 * per.getApodo());
 		 */
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/leo/xml/beans.xml");
 		Persona per = (Persona) appContext.getBean("persona");
-		
-		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo());
-		((ConfigurableApplicationContext)appContext).close();
+
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre()
+				+ " " + per.getPais().getCiudad().getNombre());
+		((ConfigurableApplicationContext) appContext).close();
 
 	}
 
